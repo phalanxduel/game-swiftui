73eb4e93d4fd27b042eea6ce05fa47aaa950a0b3
Mike Hall <mike@just3ws.com>
2026-03-16 00:53:39 -0500

    Initial Commit

---
+++ PhalanxDuelClient/PhalanxDuelClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhalanxDuelClientApp.swift
+//  PhalanxDuelClient
+//
+//  Created by Mike Hall on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PhalanxDuelClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhalanxDuelClientTests/PhalanxDuelClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhalanxDuelClientTests.swift
+//  PhalanxDuelClientTests
+//
+//  Created by Mike Hall on 3/16/26.
+//
+
+import Testing
+@testable import PhalanxDuelClient
+
+struct PhalanxDuelClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
